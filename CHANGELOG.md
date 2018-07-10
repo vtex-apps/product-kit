3b6460974f6f4a7e751a541791295bb12702a68e
Gustavo Ribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-07-10 16:17:02 -0300

    Add isLayout property to getSchema (#3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,10 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- `isLayout` property to getSchema of ProductKit component.
 
 ## [0.1.0] - 2018-7-6
-
 ### Added
 - PropTypes validation according to graphql data.
 - Initial implementation
--- react/ProductKit.js
+++ react/ProductKit.js
@@ -36,25 +36,30 @@ class ProductKit extends Component {
           type: 'boolean',
           title: 'editor.productKit.showListPrice',
           default: true,
+          isLayout: true,
         },
         showLabels: {
           type: 'boolean',
           title: 'editor.productKit.showLabels',
           default: false,
+          isLayout: true,
         },
         showInstallments: {
           type: 'boolean',
           title: 'editor.productKit.showInstallments',
           default: false,
+          isLayout: true,
         },
         showBadge: {
           type: 'boolean',
           title: 'editor.productKit.showBadge',
           default: false,
+          isLayout: true,
         },
         badgeText: showBadge ? {
           type: 'string',
           title: 'editor.productKit.badgeText',
+          isLayout: false,
         } : {},
       },
     }
