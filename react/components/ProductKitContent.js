6ebe09147e9b1f9c66801f844f65e34c9827ec46
gugabribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-10-17 13:39:21 -0300

    Add Slider to ProductKitContent component

--- react/components/ProductKitContent.js
+++ react/components/ProductKitContent.js
@@ -10,6 +10,8 @@ import defaultEqualsIcon from '../images/default-equals-icon.svg'
 
 import ProductKitSeparator from './ProductKitSeparator';
 
+import Slider from 'vtex.store-components/Slider'
+
 /**
  * ProductKitContent component.
  * Displays a list of items which composes a kit.
@@ -19,30 +21,61 @@ export default class ProductKitContent extends Component {
     const { itemsKit, allowSwap, allowRemoval, viewOptions, onItemSwap, onItemRemoval } = this.props
 
     return (
-      <div className="vtex-product-kit flex flex-column items-center justify-center mb7">
+      <div className="vtex-product-kit dn flex-ns flex-ns flex-column-ns items-center-ns justify-center-ns mb7-ns">
         <div className="b ttu f4 mv7">
           <FormattedMessage id="productKit.mountYourKit" />
         </div>
-        <div className="flex flex-column flex-wrap-l flex-row-l items-center justify-center ba b--black-05 pa5 w-100">
-          {itemsKit.map((item, index) => (
-            <div className="flex flex-row" key={index}>
-              {index > 0 && (
-                <ProductKitSeparator icon={defaultPlusIcon} />
-              )}
-              <ProductKitItem
-                item={item}
-                itemIndex={index}
-                onItemSwap={onItemSwap}
-                onItemRemoval={onItemRemoval}
-                viewOptions={viewOptions}
-                allowSwap={allowSwap && index > 0}
-                allowRemoval={allowRemoval && index > 0}
-              />
+        <Slider
+          sliderSettings={{
+            dots: true,
+            slidesToShow: 1
+          }}>
+          <div className="w-100">
+            <div className="flex flex-row items-center justify-center ba b--black-05 pa5 w-100">
+              {itemsKit.map((item, index) => (
+                <div className="flex flex-row" key={index}>
+                  {index > 0 && (
+                    <ProductKitSeparator icon={defaultPlusIcon} />
+                  )}
+                  <ProductKitItem
+                    item={item}
+                    itemIndex={index}
+                    onItemSwap={onItemSwap}
+                    onItemRemoval={onItemRemoval}
+                    viewOptions={viewOptions}
+                    allowSwap={allowSwap && index > 0}
+                    allowRemoval={allowRemoval && index > 0}
+                  />
+                </div>
+              ))}
+              {/* <ProductKitSeparator icon={defaultEqualsIcon} /> */}
+              {/* <ProductKitDetails items={itemsKit} /> */}
             </div>
-          ))}
-          {/* <ProductKitSeparator icon={defaultEqualsIcon} /> */}
-          {/* <ProductKitDetails items={itemsKit} /> */}
-        </div>
+          </div>
+
+          <div className="w-100">
+            <div className="flex flex-row items-center justify-center ba b--black-05 pa5 w-100">
+              {itemsKit.map((item, index) => (
+                <div className="flex flex-row" key={index}>
+                  {index > 0 && (
+                    <ProductKitSeparator icon={defaultPlusIcon} />
+                  )}
+                  <ProductKitItem
+                    item={item}
+                    itemIndex={index}
+                    onItemSwap={onItemSwap}
+                    onItemRemoval={onItemRemoval}
+                    viewOptions={viewOptions}
+                    allowSwap={allowSwap && index > 0}
+                    allowRemoval={allowRemoval && index > 0}
+                  />
+                </div>
+              ))}
+              {/* <ProductKitSeparator icon={defaultEqualsIcon} /> */}
+              {/* <ProductKitDetails items={itemsKit} /> */}
+            </div>
+          </div>
+        </Slider>
       </div>
     )
   }
