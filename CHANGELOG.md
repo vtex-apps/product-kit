72f593f4d846fb359f5184f300e95fcdd57d0921
Bruno Dias <bruno.dias@vtex.com.br>
2019-01-11 15:31:13 -0200

    Fix font title

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 ### Changed
-- Adjust padding of product-kit to follow the store padding. 
+- Adjust padding of product-kit to follow the store padding.
 
 ## [1.2.0] - 2019-01-09
 ### Changed
--- react/index.js
+++ react/index.js
@@ -56,7 +56,7 @@ export default class ProductKit extends Component {
     /** The component should be displayed only in large screens for a while */
     return (
       <Container className="vtex-product-kit-list dn db-ns flex-column">
-        <div className="t-heading-3 mv4 flex items-center justify-center">
+        <div className="t-heading-3 c-muted-1 fw3 mv4 flex items-center justify-center">
           <FormattedMessage id="productKitList.mountYourKit" />
         </div>
         <Slider
