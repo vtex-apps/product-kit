9454206b602302657dad03d904254e442e31b1fd
Theo Moura <theomouram@gmail.com>
2019-02-26 10:29:39 -0300

    updating icon mock and snapshots

--- react/__mocks__/vtex.dreamstore-icons.js
+++ react/__mocks__/vtex.dreamstore-icons.js
@@ -1,19 +1,15 @@
 import React from 'react'
 
-const iconMock = (orientation, size, className, name) => {
+const iconMock = (size, className, name) => {
   return (
-    <svg
-      className={`${orientation}-oritentation-mock ${className} ${name}`}
-      width={size}
-      height={size}
-    >
+    <svg className={`${className} ${name}`} width={size} height={size}>
       <rect width={size} height={size} />
     </svg>
   )
 }
 
-export const IconEquals = ({ orientation, size, className }) =>
-  iconMock(orientation, size, className, 'IconEquals')
+export const IconEquals = ({ size, className }) =>
+  iconMock(size, className, 'IconEquals')
 
-export const IconPlus = ({ orientation, size, className }) =>
-  iconMock(orientation, size, className, 'IconPlus')
+export const IconPlus = ({ size, className }) =>
+  iconMock(size, className, 'IconPlus')
